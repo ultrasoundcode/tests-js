26e570f623b4b3965c791a52fe056f5f0805cd61
Askhat Anarbayev <0x.ultrasound@gmail.com>
2023-03-29 13:14:22 +0600

    Update main.js
    refactoring

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,24 @@
 import './styles/style.css';
 import axios from 'axios';
 const allProductsList = document.querySelector('#allProducts');
-const getProducts = axios
-  .get('https://dummyjson.com/products')
-  .then((response) => response.data)
-  .then((data) => {
-    console.log(data);
-    const murkUp = data.products
-      .map(({ title, price, description }) => {
-        return `<li>
+
+const createMurkUp = (data) => {
+  console.log(data);
+  const murkUp = data.products
+    .map(({ title, price, description }) => {
+      return `<li>
 				          <h3>${title}</h3>
 				          <p>${price}</p>
 				          <p>${description}</p>
 			         </li>`;
-      })
-      .join('');
-    return murkUp;
-  })
+    })
+    .join('');
+  return murkUp;
+};
+const getProducts = axios
+  .get('https://dummyjson.com/products')
+  .then((response) => response.data)
+  .then(createMurkUp)
   .then((murkUp) => {
     allProductsList.insertAdjacentHTML('beforeend', murkUp);
   });
